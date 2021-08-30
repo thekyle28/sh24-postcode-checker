d2590b7bbe36e220546e714fb93af7b5c0c0d130
Kyle Allen-Taylor <karat28@hotmail.co.uk>
2021-08-30 09:59:03 +0100

    Update test_case file to run system tests, not working with WSL

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,31 @@
-require "test_helper"
+require 'test_helper'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
+require 'selenium-webdriver'
+require 'webdrivers/chromedriver'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  Capybara.asset_host = 'http://localhost:3000'
+  CAPYBARA_WINDOW_SIZE = [1366, 768].freeze
+  Capybara.register_driver :chrome_headless do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument("headless")
+    options.add_argument("window-size=#{CAPYBARA_WINDOW_SIZE.join(',')}")
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options
+    )
+  end
+  Capybara.javascript_driver = :chrome
+  Capybara.ignore_hidden_elements = false
+  Capybara.save_path = "spec/screenshots"
+  Capybara::Screenshot.autosave_on_failure = false
+  
+  Capybara::Screenshot.prune_strategy = :keep_last_run
+  Capybara::Screenshot.register_driver(:chrome) do |driver, path|
+    driver.browser.save_screenshot(path)
+  end
+
 end
